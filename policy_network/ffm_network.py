822a9d20c76e12fa67d88eee81d76b72bd19148a
nphard6868 <nphard6868@gmail.com>
2021-02-14 17:42:02 +0800

    save

--- policy_network/ffm_network.py
+++ policy_network/ffm_network.py
@@ -58,25 +58,62 @@ def build_policy_fn(config, name):
             order_2_emb_list.append(emb_sum)
 
         order_2 = tf.concat(order_2_emb_list, axis=-1)
+
+        weight_list = []
+
+        for fea in fea_conf.get('order_1'):
+            fea_name = fea.get('name')
+            bucket_size = fea.get('bucket_size')
+            with tf.variable_scope('order_1', reuse=tf.AUTO_REUSE):
+                weights = tf.get_variable(fea_name,
+                                          [bucket_size, 1],
+                                          initializer=tf.constant_initializer(0.1))
+
+                ids = tf.string_to_hash_bucket_fast(features.get(fea_name), bucket_size)
+                weight = tf.nn.embedding_lookup(weights, ids, name=fea_name + '_lookup')
+                weight_list.append(weight)
+
+        for field in fea_conf.get('fields'):
+            field_name = field.get('field_name')
+            emb_size = field.get('embedding_size')
+            fea_num = len(field.get('features'))
+            if fea_num > 1:
+                in_field_emb_list = []
+                for fea in field.get('features'):
+                    fea_name = fea.get('name')
+                    bucket_size = fea.get('bucket_size')
+                    with tf.variable_scope(name + '_in_filed', reuse=tf.AUTO_REUSE):
+                        embedding = tf.get_variable(field_name + '_' + fea_name,
+                                                    [bucket_size, emb_size],
+                                                    initializer=tf.random_uniform_initializer(-0.1, 0.1))
+
+                        ids = tf.string_to_hash_bucket_fast(features.get(fea_name), bucket_size)
+                        emb = tf.nn.embedding_lookup(embedding, ids, name=fea_name + '_lookup')
+                        in_field_emb_list.append(tf.expand_dims(emb, axis=1))
+
+                sum_then_square_emb = tf.reduce_sum(tf.square(
+                    tf.reduce_sum(tf.concat(in_field_emb_list, axis=1), axis=1)
+                ), axis=-1, keep_dims=True)
+                square_then_sum_emb = tf.reduce_sum(
+                    tf.reduce_sum(tf.square(tf.concat(in_field_emb_list, axis=1)), axis=-1), axis=-1,
+                    keep_dims=True)
+                in_field_comb_weight = 0.5 * (sum_then_square_emb - square_then_sum_emb)
+                weight_list.append(in_field_comb_weight)
+
+        one_weight = tf.reduce_sum(tf.concat(weight_list, axis=-1), axis=-1, keepdims=True)
         shape = tf.shape(order_2)
 
-        # if name == 'user':
-        #     pad_vec = tf.tile(tf.expand_dims(tf.constant([1], dtype=tf.float32), axis=0),
-        #                       [shape[0], 1])
-        #
-        #     res_vec = tf.concat([order_1, pad_vec, order_2], axis=-1)
-        #
-        # else:
-        #     with tf.variable_scope('bias', reuse=tf.AUTO_REUSE):
-        #         bias = tf.get_variable('item_bias', [1, 1],
-        #                                trainable=False,
-        #                                initializer=tf.constant_initializer(1.0))
-        #         bias_fea = tf.tile(bias, [shape[0], 1])
-        #
-        #     pad_vec = tf.tile(tf.expand_dims(tf.constant([1], dtype=tf.float32), axis=0),
-        #                       [shape[0], 1])
-        #
-        #     res_vec = tf.concat([bias_fea, pad_vec, order_1, order_2], axis=-1)
-        return order_2
+        if name == 'user':
+            pad_vec = tf.tile(tf.expand_dims(tf.constant([1], dtype=tf.float32), axis=0),
+                              [shape[0], 1])
+
+            res_vec = tf.concat([one_weight, pad_vec, order_2], axis=-1)
+
+        else:
+            pad_vec = tf.tile(tf.expand_dims(tf.constant([1], dtype=tf.float32), axis=0),
+                              [shape[0], 1])
+
+            res_vec = tf.concat([pad_vec, one_weight, order_2], axis=-1)
+        return res_vec
 
     return policy_fn
